ccd164a7dbbd5b05f27a05b1bad08e8e6380f7a0
Eric Oestrich <eric@oestrich.org>
2019-12-24 16:25:42 -0500

    Release script to generate elixir release

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+rm -rf tmp/build
+mkdir -p tmp/build
+git archive --format=tar master | tar x -C tmp/build/
+cd tmp/build
+
+docker build -f Dockerfile.releaser -t stein_example:releaser .
+
+DOCKER_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+docker run -ti --name stein_example_releaser_${DOCKER_UUID} stein_example:releaser /bin/true
+docker cp stein_example_releaser_${DOCKER_UUID}:/opt/stein_example.tar.gz ../
+docker rm stein_example_releaser_${DOCKER_UUID}
