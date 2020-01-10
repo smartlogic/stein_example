497c2be68745e055935e5b6f632741f4caeeced4
Eric Oestrich <eric@oestrich.org>
2020-01-10 14:38:24 -0500

    Add secret key base to the releases.exs
    Remove prod.secret.exs

--- config/releases.exs
+++ config/releases.exs
@@ -1,12 +1,14 @@
 import Config
 
 config :stein_example, SteinExample.Repo,
+  ssl: true,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 config :stein_example, Web.Endpoint,
   http: [port: String.to_integer(System.get_env("PORT"))],
   url: [host: System.get_env("HOST"), port: 443, scheme: "https"],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 
