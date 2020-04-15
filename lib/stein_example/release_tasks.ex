d5e3e02a43a378a2471bb1b6ab879c7c28b8292a
Eric Oestrich <eric@oestrich.org>
2020-04-15 15:18:56 -0400

    Split out release tasks to separate modules
    Prepare for extra tasks by breaking them apart

--- lib/stein_example/release_tasks.ex
+++ lib/stein_example/release_tasks.ex
@@ -1,4 +1,4 @@
-# From https://github.com/bitwalker/distillery/blob/master/docs/Running%20Migrations.md
+# Loosely from https://github.com/bitwalker/distillery/blob/master/docs/Running%20Migrations.md
 defmodule SteinExample.ReleaseTasks do
   @moduledoc false
 
@@ -10,26 +10,11 @@ defmodule SteinExample.ReleaseTasks do
     :ecto_sql
   ]
 
-  @apps [
-    :stein_example
-  ]
-
   @repos [
     SteinExample.Repo
   ]
 
-  def migrate() do
-    startup()
-
-    # Run migrations
-    Enum.each(@apps, &run_migrations_for/1)
-
-    # Signal shutdown
-    IO.puts("Success!")
-    :init.stop()
-  end
-
-  defp startup() do
+  def startup() do
     IO.puts("Loading stein_example...")
 
     # Load the code for stein_example, but don't start it
@@ -43,12 +28,34 @@ defmodule SteinExample.ReleaseTasks do
     IO.puts("Starting repos..")
     Enum.each(@repos, & &1.start_link(pool_size: 2))
   end
+end
+
+defmodule SteinExample.ReleaseTasks.Migrate do
+  @moduledoc """
+  Migrate the database
+  """
+
+  alias SteinExample.ReleaseTasks
+  alias SteinExample.Repo
+
+  @apps [
+    :stein_example
+  ]
+
+  @doc """
+  Migrate the database
+  """
+  def run() do
+    ReleaseTasks.startup()
+    Enum.each(@apps, &run_migrations_for/1)
+    IO.puts("Success!")
+  end
 
   def priv_dir(app), do: "#{:code.priv_dir(app)}"
 
   defp run_migrations_for(app) do
     IO.puts("Running migrations for #{app}")
-    Ecto.Migrator.run(SteinExample.Repo, migrations_path(app), :up, all: true)
+    Ecto.Migrator.run(Repo, migrations_path(app), :up, all: true)
   end
 
   defp migrations_path(app), do: Path.join([priv_dir(app), "repo", "migrations"])
