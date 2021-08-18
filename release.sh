ad89d67add89ae8c58838ace36b742b6b847534f
Eric Oestrich <eric@oestrich.org>
2021-08-18 15:40:29 -0400

    Copy out a REVISION file from releasing the app

--- release.sh
+++ release.sh
@@ -6,15 +6,22 @@ if [[ $(pwd) == *"/deploy" ]]; then
   cd ..
 fi
 
+rm -f deploy/tmp/REVISION deploy/tmp/stein_example.tar.gz
+
+GIT_SHA=$(git rev-parse main)
+
 rm -rf tmp/build
 mkdir -p tmp/build
 git archive --format=tar main | tar x -C tmp/build/
 cd tmp/build
 
 export DOCKER_BUILDKIT=0
-docker build -f Dockerfile.releaser -t stein_example:releaser .
+docker build --build-arg GIT_SHA=${GIT_SHA} -f Dockerfile.releaser -t stein_example:releaser .
+
+mkdir -p ../../deploy/tmp
 
 DOCKER_UUID=$(uuidgen)
 docker run --name stein_example_releaser_${DOCKER_UUID} stein_example:releaser /bin/true
 docker cp stein_example_releaser_${DOCKER_UUID}:/opt/stein_example.tar.gz ../../deploy/tmp
+docker cp stein_example_releaser_${DOCKER_UUID}:/opt/REVISION ../../deploy/tmp
 docker rm stein_example_releaser_${DOCKER_UUID}
